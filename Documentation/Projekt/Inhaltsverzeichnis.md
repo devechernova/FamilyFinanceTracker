734569ef2123b858d053328c035545b410e667eb
Ekaterina Chernova <devechernova@gmail.com>
2026-05-23 15:32:42 +0200

    Add complete project documentation including introduction, project goals, functionality, planning, architecture, implementation, problem solving, design decisions, screenshots, timeline and sources

---
+++ Documentation/Projekt/Inhaltsverzeichnis.md
@@ -0,0 +1,11 @@
+1.  Einleitung
+2.  Ziel des Projekts
+3.  Funktionalität der Anwendung
+4.  Projektplanung
+5.  Architektur der Anwendung
+6.  Implementierung
+7.  Probleme und Lösungen
+8.  Entscheidungsbegründung
+9.  Screenshots
+10. Zeitplanung
+11. Quellen
--- obj/Debug/net9.0/FamilyFinanceTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/FamilyFinanceTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyFinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f294b8a887aa9672a0041fe179be2247c6d7b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50696683320788bf181ea1f73dcc5c4c04af6ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyFinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyFinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
